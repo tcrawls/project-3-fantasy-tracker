c4c5474ce181a4741946c34138e109edce20fff8
Thompson Rawls <tcrawls@gmail.com>
2019-07-20 12:18:14 -0400

    implemented React Router on App.js

--- client/src/App.js
+++ client/src/App.js
@@ -1,14 +1,17 @@
 import React from 'react';
 import { BrowserRouter as Router, Switch, Route } from 'react-router-dom'
-import HelloWorld from './components/HelloWorld.js'
+import Teams from './components/Teams.js'
 import './App.css';
 
 function App() {
   return (
     <div className="App">
+      <div>Home</div>
+      <div>About</div>
+      <h1>Fantasy Tracker</h1>
       <Router>
         <Switch>
-          <Route exact path="/" component={HelloWorld}/>
+          <Route exact path="/" component={Teams} />
         </Switch>
       </Router>
     </div>
