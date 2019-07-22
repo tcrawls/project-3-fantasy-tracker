360510d8f671b49f4037fb7e981ac88152456bf3
Thompson Rawls <tcrawls@gmail.com>
2019-07-22 11:04:36 -0400

    implemented Edit Team functionality

--- client/src/components/SingleTeam.js
+++ client/src/components/SingleTeam.js
@@ -6,7 +6,8 @@ export default class SingleTeam extends Component {
 
     state = {
         team: {},
-        redirectToHome: false
+        redirectToHome: false,
+        isEditFormDisplayed: false
     }
 
     /* Step 4
@@ -30,15 +31,60 @@ export default class SingleTeam extends Component {
             })
     }
 
+    handleToggleEditForm = () => {
+        this.setState((state) => {
+            return { isEditFormDisplayed: !state.isEditFormDisplayed }
+        })
+    }
+
+    handleSubmit = () => {
+        axios.put(`/api/teams/${this.state.team._id}`, this.state.team)
+        .then((res) => {
+            this.setState( {team: res.data, isEditFormDisplayed: false})
+        })
+    }
+
+    handleInputChange = (event) => {
+        const copiedTeam = { ...this.state.team }
+        copiedTeam[event.target.name] = event.target.value
+        this.setState({ team: copiedTeam })
+    }
+
  
     render() {
         if (this.state.redirectToHome) {
             return <Redirect to="/" />
         }
         return (
+            this.state.isEditFormDisplayed
+                ?
+                <form onSubmit={this.handleSubmit}>
+                <div>
+                    <label htmlFor="team-name">Team Name: </label>
+                    <input type="text" id="team-name" name="name" onChange={this.handleInputChange} value={this.state.team.name} />
+                </div>
+                <div>
+                    <label htmlFor="platform">Platform: </label>
+                    <input type="text" id="platform" name="platform" onChange={this.handleInputChange} value={this.state.team.platform} />
+                </div>
+                <div>
+                    <label htmlFor="scoringFormat">Scoring Format: </label>
+                    <input type="text" id="scoringFormat" name="scoringFormat" onChange={this.handleInputChange} value={this.state.team.scoringFormat} />
+                </div>
+                <div>
+                    <label htmlFor="record">Current Record: </label>
+                    <input type="string" id="record" name="record" onChange={this.handleInputChange} value={this.state.team.record} />
+                </div>
+                <div>
+                    <label htmlFor="icon">Icon Image: </label>
+                    <input type="text" id="icon" name="icon" onChange={this.handleInputChange} value={this.state.team.icon} />
+                </div>
+                <input type="submit" value="Update Team" />
+            </form>
+                :
             <div>
                 <h2>{this.state.team.name}</h2>
-                <button>Edit Team</button>
+                <button onClick={this.handleToggleEditForm}>Edit Team</button>
                 <button onClick={this.handleDeleteTeam}>Delete Team</button>
                 <div>Platform: {this.state.team.platform}</div>
                 <div>Scoring Format: {this.state.team.scoringFormat}</div>
