2b163c8a8bd33161bd1ea242b17320cb76c94a02
Thompson Rawls <tcrawls@gmail.com>
2019-07-25 16:16:54 -0400

    configured for Heroku deployment

---
+++ client/build/precache-manifest.3946d75d9a9031e8d6ce0541e7dd3c88.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4b0a1c29279f1979efcf32fde3ac3638",
+    "url": "/index.html"
+  },
+  {
+    "revision": "8df3a70cf7276352de1e",
+    "url": "/static/css/main.2cce8147.chunk.css"
+  },
+  {
+    "revision": "f8d2d9344fe2689b427f",
+    "url": "/static/js/2.2f9dfdf9.chunk.js"
+  },
+  {
+    "revision": "8df3a70cf7276352de1e",
+    "url": "/static/js/main.69d8f15f.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  }
+]);
